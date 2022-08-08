53902b6d6d6c05c709c875af8e2c568eb1f6834f
Riaz <81258483+R14Z@users.noreply.github.com>
2022-08-08 16:26:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,97 @@
+one = 1000
+
+level = 0
+
+# things that you need to get more exp
+
+# exercise 
+
+exercise = input('Have you done any exercise today (Y,N) ').upper()
+
+if exercise == 'Y':
+    # the amount of exercise
+    pushups_fifty = 50
+    pushups_eighty = 100
+    pushups_hundred = 150
+    weights = 150
+
+    # exercise node
+    exercise_q = input('Have you done push-ups or weights? (P,W,B) ').upper()
+    if exercise_q == 'P':
+        pushup_q = int((input('How many have you done? (50, 80, 100) ')))
+        if pushup_q == 50:
+            level = level + pushups_fifty
+        elif pushup_q == 80:
+            level = level + pushups_eighty
+        elif pushup_q == 150:
+            level = level + pushups_hundred
+        else:
+            print('Bye!')
+    if exercise_q == 'W':
+        level = level + weights
+    if exercise_q == 'B':
+        print('Damn, good job. You earned a 2x bonus.')
+        level = level + 300
+else:
+    print('Do it. Now. I thought you wanted to be the strongest in your bloodline. No? ')
+
+# meditating
+
+meditating = 100 
+
+meditating_q = input('Have you meditated today? (Y,N) ')
+if meditating_q == 'Y':
+    print('Good job, you have earned 100xp.')
+    level = level + 100
+else:
+    print('Okay, do it.')
+
+# social media
+
+social = 100
+
+social_q = input('Have you used social media for less than 50 minutes today? (Y,N) ')
+if social_q == 'Y':
+    print('Good job. Nice little dopamine detox. ')
+    level = level + social
+else:
+    print('Better luck next time.')
+
+# nature 
+
+nature = 200
+
+nature_q = input('Have you explored nature today? (Y,N) ')
+if nature_q == 'Y':
+    print('Good job.')
+    level = level + nature
+else:
+    print("It's always good to explore nature, maybe next time.")
+
+
+# journalizing
+
+journalize = 50
+journalize_phone = 70
+
+journalize_q = input('Have you journalized today? (Y,N) ')
+if journalize_q == 'Y':
+    device = input('Have you journalized on a phone or another device? (P,O) ')
+    if device == 'P':
+        print('Nice! You earned a 20xp bonus for journalizing on phone.')
+        level = level + journalize_phone
+    else:
+        print('Good job. You earned 50xp. ')
+        level = level + journalize_phone
+
+# clean
+
+clean = 80
+
+clean_q = input('Have you cleaned anything? (Y,N) ')
+if clean_q == 'Y':
+    print('Nice, helping out your parents.')
+    level = level + clean
+else:
+    print('Do it next time. NEXT TIME.')
+
